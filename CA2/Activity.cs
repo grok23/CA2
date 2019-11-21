b36fb9ad2df5c3e3c6a8c3c4aa29a8bda078879e
grok23 <46896171+grok23@users.noreply.github.com>
2019-11-21 12:25:00 +0000

    added basics of Activity class

---
+++ CA2/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public enum ActivityType { Air, Water, Land } 
+
+    class Activity
+    {
+        public string Name { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public decimal Cost { get; set; }
+        private string _Description;
+        public string Description
+        {
+            get { return _Description + "Cost - {Cost}"; }
+            set { _Description = value; }
+        }
+        public ActivityType TypeOfActivity { get; set; }
+
+
+        }
+}
